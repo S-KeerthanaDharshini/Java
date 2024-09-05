2bb750e2f2dc9667ee7877787ae70f8338900c15
keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2024-09-05 21:44:44 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,47 @@
+/*  Adding up the consecutive lettere=s which is even in count
+ * 
+ * Ex:
+ * Input 1:
+ * aaaabbcbbbdd
+ * Output 1:
+ * 8
+ * 
+ */
+
+
+
+
+
+import java.util.*;
+
+public class Count{
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String str=sc.next();
+    
+    int N=str.length();
+    int i=0;
+   // int j=0;
+    int count=0;
+    int maxcount=0;
+    while(i<=N/2){
+      
+        count=1;
+        for(int j=i+1;j<N;j++){
+            if(str.charAt(i)== str.charAt(j)){
+                count++;
+            }
+            else{
+                if(count % 2==0){
+                    maxcount=maxcount+count;
+                }
+                i=j;
+                //break;
+            }
+        
+        }
+    }
+    System.out.println(maxcount);
+    sc.close();
+}
+}
